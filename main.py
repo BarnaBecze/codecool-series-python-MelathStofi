861de4c36309ef153edc28a79bce6fecb720e3e8
MelathStofi <melath.stofi@gmail.com>
2019-08-20 17:01:18 +0200

    add layout.html and create route and link for 15 most rated shows

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def design():
     return render_template('design.html')
 
 
+@app.route('/15-most-rated-shows')
+def _15_most_rated_shows():
+    shows = queries.get_15_most_rated_shows()
+    return render_template('15_most_rated_shows.html', shows=shows)
+
+
 def main():
     app.run(debug=True)
 
