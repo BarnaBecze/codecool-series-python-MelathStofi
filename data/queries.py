5d2560b8af009bbd003f8050f026b14a112eaad3
Barna Becze <barna.bcz@gmail.com>
2019-08-30 23:04:36 +0200

    Modify select query to include all necessary information for 1st card

--- data/queries.py
+++ data/queries.py
@@ -2,4 +2,10 @@ from data import data_manager
 
 
 def get_shows():
-    return data_manager.execute_select('SELECT id, title FROM shows;')
+    return data_manager.execute_select('''
+                                        SELECT title, year, rating, homepage, trailer, runtime, STRING_AGG(g.name, ', ') AS genre
+                                        FROM shows
+                                        JOIN show_genres sg on shows.id = sg.show_id
+                                        JOIN genres g on sg.genre_id = g.id
+                                        GROUP BY shows.id
+                                        ORDER BY rating DESC;''')
