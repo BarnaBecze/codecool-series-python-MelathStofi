49c90732ea8152cb0b1fe3b02460f552463fbf07
Barna Becze <barna.bcz@gmail.com>
2019-08-31 17:07:59 +0200

    Add function to query show details

--- data/queries.py
+++ data/queries.py
@@ -1,4 +1,5 @@
 from data import data_manager
+from json import dumps
 
 
 def get_shows():
@@ -9,3 +10,14 @@ def get_shows():
                                         JOIN genres g on sg.genre_id = g.id
                                         GROUP BY shows.id
                                         ORDER BY rating DESC;''')
+
+
+def get_show_details(title):
+    return data_manager.execute_select(f"""
+                                        SELECT title, year, rating, runtime, overview, STRING_AGG(g.name, ', ') AS genre
+                                        FROM shows
+                                        JOIN show_genres sg on shows.id = sg.show_id
+                                        JOIN genres g on sg.genre_id = g.id
+                                        WHERE title = '{title}'
+                                        GROUP BY shows.id;
+                                        """)
